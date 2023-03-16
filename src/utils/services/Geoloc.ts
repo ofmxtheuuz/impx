44760a68480258de9eeecd353848d07d81ffa9c5
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-03-16 14:05:58 -0300

    Finder completed with ipapi.co!

--- src/utils/services/Geoloc.ts
+++ src/utils/services/Geoloc.ts
@@ -1,4 +1,11 @@
-﻿
-function getLoc(addr: string) {
-    
+﻿import axios from "axios";
+import {GetLocInterface} from "../../interfaces/GetLoc.interface";
+
+const BASE_URL = "https://ipapi.co/ip/json/"
+
+async function getLoc(addr: string): Promise<GetLocInterface> {
+    if(addr != null && addr.length > 0 && addr.includes(".")) {
+        const result = await axios(BASE_URL.replace("ip", addr))
+        return result.data
+    }
 }
